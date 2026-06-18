a1a2f3bad1dedcaed296324df0b19967a49b6355
Jonny Nilsson <jompa67@gmail.com>
2026-06-18 11:04:18 +0700

    First version

---
+++ GridCalc.App/CryptoExchange/BinanceExchange.cs
@@ -0,0 +1,30 @@
+﻿using Binance.Net;
+using Binance.Net.Clients;
+using Binance.Net.Interfaces.Clients.SpotApi;
+
+namespace GridCalc.App.CryptoExchange;
+
+public class BinanceExchange
+{
+    private BinanceRestClient exchangeClient;
+    private BinanceSocketClient exchangeSocketClient;
+
+    public async Task<IBinanceRestClientSpotApiShared> Initialize(string key, string secret)
+    {
+        exchangeClient = new BinanceRestClient(options =>
+        {
+            options.ApiCredentials = new BinanceCredentials(key, secret);
+            options.Environment = BinanceEnvironment.Testnet;
+            options.OutputOriginalData = true;
+        });
+
+        exchangeSocketClient = new BinanceSocketClient(options =>
+        {
+            options.ApiCredentials = new BinanceCredentials(key, secret);
+            options.Environment = BinanceEnvironment.Testnet;
+            options.OutputOriginalData = true;
+        });
+        
+        return exchangeClient.SpotApi.SharedClient;
+    }
+}
